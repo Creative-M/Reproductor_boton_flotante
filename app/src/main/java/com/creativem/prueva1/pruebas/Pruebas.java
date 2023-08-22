c3d18c178bbcc5fe3be4f6c7e4f8d21a280292f0
SamiGaming <104452588+SamiGamin@users.noreply.github.com>
2023-08-22 18:54:36 -0500

    2

---
+++ app/src/main/java/com/creativem/prueva1/pruebas/Pruebas.java
@@ -0,0 +1,16 @@
+package com.creativem.prueva1.pruebas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.creativem.prueva1.R;
+
+public class Pruebas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pruebas);
+    }
+}
